e9cc87ea979d87749f0c25e91117502df4ef81f5
e-reyno <110035645+e-reyno@users.noreply.github.com>
2022-07-27 13:38:13 +0100

    Add files via upload

--- pi_monte_carlo_edits.py
+++ pi_monte_carlo_edits.py
@@ -11,6 +11,9 @@ from time import perf_counter
 
 def pi_estimate(counts):
     
+    if isinstance(counts, str):
+        raise TypeError("Only integers can be passed for number of counts")
+
     inside_count = 0
     inside_x = np.array([])
     inside_y = np.array([])
@@ -19,8 +22,8 @@ def pi_estimate(counts):
     
     for i in range(counts):
         
-        x = np.random.uniform(-1, 1)
-        y = np.random.uniform(-1, 1)
+        x = generate_random_number(-1, 1)
+        y = generate_random_number(-1, 1)
         radius = np.sqrt(x**2 + y**2)
         
         if (radius<1):
@@ -37,10 +40,12 @@ def pi_estimate(counts):
     pi_estimatation = 4 * inside_count / counts
     return pi_estimatation, inside_x, inside_y, outside_x, outside_y
 
-def plot_circle(counts):
+def generate_random_number(low, high):
+    
+    return np.random.uniform(-1,1)
 
-    pi, inside_x, inside_y, outside_x, outside_y = pi_estimate(counts)
-    print("The estimate of pi for ", counts, " counts is ", pi)
+def plot_circle(inside_x, inside_y, outside_x, outside_y):
+
     plt.figure(figsize=(5, 5))
     plt.xlabel('x')
     plt.ylabel('y')
@@ -75,15 +80,24 @@ def plot_counts_pi(counts_array, pi_array):
     
     return None
 
-number_counts = 10000
-plot_circle(number_counts)
+def main():
+    
+    number_counts = 10000
+    pi, inside_x, inside_y, outside_x, outside_y = pi_estimate(number_counts)
+    print("The estimate of pi for ", number_counts, " counts is ", pi)
+    plot_circle(inside_x, inside_y, outside_x, outside_y)
 
-#start the counter for checking runtime for varying the number of counts
-time_start = perf_counter()
-number_counts_array = np.arange(1, 10000, 100)
-pi_estimates = pi_varying_counts(number_counts_array)
-time_end = perf_counter()
+    #start the counter for checking runtime for varying the number of counts
+    time_start = perf_counter()
+    number_counts_array = np.arange(1, 10000, 100)
+    pi_estimates = pi_varying_counts(number_counts_array)
+    time_end = perf_counter()
 
-plot_counts_pi(number_counts_array, pi_estimates)
-print("Total runtime to find all Pi Estimates is  {0:0.5f} seconds".format(
-        time_end-time_start))
+    plot_counts_pi(number_counts_array, pi_estimates)
+    print("Total runtime to find all Pi Estimates is  {0:0.5f} seconds".format(
+            time_end-time_start))
+    
+    return None
+
+if __name__ == '__main__':
+    main()
