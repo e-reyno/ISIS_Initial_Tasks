f43bd4ed0bde1ec1b61d1fc6c188602b0bfbf736
e-reyno <110035645+e-reyno@users.noreply.github.com>
2022-07-28 15:07:43 +0100

    Update pi_monte_carlo_edits.py
    added radius function

--- pi_monte_carlo_edits.py
+++ pi_monte_carlo_edits.py
@@ -4,13 +4,14 @@ Created on Mon Jul 25 15:47:27 2022
 
 Script for estimating pi using monte carlo simulation
 """
+from time import perf_counter
+import math
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-from time import perf_counter
+
 
 def pi_estimate(counts):
-    
+
     if isinstance(counts, str):
         raise TypeError("Only integers can be passed for number of counts")
 
@@ -19,30 +20,34 @@ def pi_estimate(counts):
     inside_y = np.array([])
     outside_x = np.array([])
     outside_y = np.array([])
-    
-    for i in range(counts):
-        
+
+    for _ in range(counts):
+
         x = generate_random_number(-1, 1)
         y = generate_random_number(-1, 1)
-        radius = np.sqrt(x**2 + y**2)
-        
-        if (radius<1):
-        #true for if inside circle 
+        radius = find_radius(x, y)
+
+        if radius < 1:
+        #true for if inside circle
             inside_x = np.append(inside_x, x)
             inside_y = np.append(inside_y, y)
             inside_count += 1
 
-        elif (radius>1):
+        else:
             outside_x = np.append(outside_x, x)
             outside_y = np.append(outside_y, y)
 
     #pi is the area of a circle with a radius of 1
     pi_estimatation = 4 * inside_count / counts
+    #assert (inside_count+outside_count == counts)
     return pi_estimatation, inside_x, inside_y, outside_x, outside_y
 
+def find_radius(x, y):
+    return np.sqrt(x**2+y**2)
+
 def generate_random_number(low, high):
-    
-    return np.random.uniform(-1,1)
+
+    return np.random.uniform(low, high)
 
 def plot_circle(inside_x, inside_y, outside_x, outside_y):
 
@@ -54,34 +59,34 @@ def plot_circle(inside_x, inside_y, outside_x, outside_y):
     plt.Circle((0, 0), 0.5, color='r')
     plt.title("Accepted or rejected points for a circle of radius 1")
     plt.show()
-    
+
     return None
 
 def pi_varying_counts(counts_array):
-    
+
     pi_array = np.array([])
 
     for counts in counts_array:
-        
+
         estimate_values = pi_estimate(counts)
         pi_array = np.append(pi_array, estimate_values[0])
-        
+
     return pi_array
 
 def plot_counts_pi(counts_array, pi_array):
-    
-    plt.figure(figsize=(5,5))
+
+    plt.figure(figsize=(5, 5))
     plt.plot(counts_array, pi_array)
     plt.title('Pi Estimate vs Number of Counts')
     plt.xlabel('Number of Counts')
     plt.ylabel('Pi Estimate')
     plt.axhline(y=math.pi, color='r')
     plt.show()
-    
+
     return None
 
 def main():
-    
+
     number_counts = 10000
     pi, inside_x, inside_y, outside_x, outside_y = pi_estimate(number_counts)
     print("The estimate of pi for ", number_counts, " counts is ", pi)
@@ -95,9 +100,9 @@ def main():
 
     plot_counts_pi(number_counts_array, pi_estimates)
     print("Total runtime to find all Pi Estimates is  {0:0.5f} seconds".format(
-            time_end-time_start))
-    
-    return None
+        time_end-time_start))
+
+    return 1
 
 if __name__ == '__main__':
     main()
