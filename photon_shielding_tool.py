b1592a871b3cafcb3274753f0f3a3e22f980ec5d
e-reyno <elliot.reynolds@stfc.ac.uk>
2022-08-01 12:07:36 +0100

    updated densities function

--- photon_shielding_tool.py
+++ photon_shielding_tool.py
@@ -16,23 +16,28 @@ def read_file(filename):
 #find hvl
 def convert_hvl(mass_attenuations, densities):
 
-    lead_mass = densities[0] * mass_attenuations.loc[:,"Lead"]
-    concrete_mass = densities[1] * mass_attenuations.loc[:,"Concrete"]
-    iron_mass = densities[2] * mass_attenuations.loc[:,"Iron"]
-    d = pd.DataFrame({'Lead':a, 'Concrete':b, "Iron":c})   
-
-    return np.log(2) / d
-
-material_values = read_file("material_values.csv")
+    attenuation_lead = densities[0] * mass_attenuations.loc[:,"Lead"]
+    attenuation_concrete = densities[1] * mass_attenuations.loc[:,"Concrete"]
+    attenuation_iron = densities[2] * mass_attenuations.loc[:,"Iron"]
+    attenuations = pd.DataFrame({'Lead':attenuation_lead, 'Concrete':attenuation_concrete, "Iron":attenuation_iron})
+    return np.log(2) / attenuations
 
 density_lead = 11.29 #gram/cm3
 density_iron =7.874 #gram/c3
 density_concrete = 2.4 #gram/cm3
+
+material_values = read_file("material_values.csv")
+
 density_materials = np.array([density_lead, density_concrete, density_iron])
 #convert to half value thicknesses
 material_values.loc[:,("Lead","Concrete", "Iron")] = convert_hvl(material_values.loc[:,("Lead","Concrete", "Iron")], density_materials)
 
+
+
 number_photons = 1e3
+
+#multiply thicknesses for number of photons?
+
 material_values.plot(kind='scatter',x='Energy', y='Lead')
 material_values.plot(kind='scatter',x='Energy', y='Concrete')
 material_values.plot(kind='scatter',x='Energy', y='Iron')
